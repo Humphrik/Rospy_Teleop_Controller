df97ab11cdbcb1c09a8866238aac7c8f69e2d8b8
Kenneth Humphries <humphrik@uchicago.edu>
2021-03-16 14:07:32 -0500

    Phi rotation added

--- scripts/drive_system.py
+++ scripts/drive_system.py
@@ -35,38 +35,18 @@ class Robot_Controller:
         # print ("Right js value changed to ", LRval)
         self.cmd_pub.publish(Vector3(self.robot_vel, 0, 0), Vector3(0, 0, self.robot_turn))
 
-    def onRightBtn2Change(self, val):
-        if (val > 0):
-            self.robot_group_arm_curr[0] += -val
-            self.move_group_arm.go(self.robot_group_arm, wait=True)
-            self.move_group_arm.stop()
+    def onRightBtn1Change(self, val):
+        self.z_dir = val
         
-    def onLeftBtn2Change(self, val):
-        if (val > 0):
-            self.robot_group_arm_curr[0] += val
-            self.move_group_arm.go(self.robot_group_arm, wait=True)
-            self.move_group_arm.stop()
+        
+    def onLeftBtn1Change(self, val):
+        self.z_dir = -val
 
     def onHatChange(self, LRval, UDval):
-        # L1 ~ 130, L2 ~ 130 mm
-        L = 130
-        denom = 2*L*L
-        x = self.robot_arm_curr_pos[1] + LRval
-        y = self.robot_arm_curr_pos[2] + UDval
-        d = x**2 + y**2
-        numer = denom - d
-        new_q2_1 = np.pi - math.acos(numer/denom)
-        new_q2_2 = math.acos(-numer/denom)
-        new_q1_1 = math.atan(y/x) - math.atan((L*math.sin(new_q2_1))/(L + L*math.cos(new_q2_1)))
-        new_q1_2 = math.atan(y/x) - math.atan((L*math.sin(new_q2_2))/(L + L*math.cos(new_q2_2)))
-        #print(x, y, '\n', self.robot_arm_curr_pos[3], new_q2_1, new_q2_2, '\n', self.robot_arm_curr_pos[4], new_q1_1, new_q1_2)
-        self.robot_arm_curr_pos = [d, x, y, new_q1_1, new_q2_1]
-        self.robot_group_arm_curr[1] = new_q1_1
-        self.robot_group_arm_curr[2] = math.radians(90) - new_q2_1
-        self.move_group_arm.go(self.robot_group_arm_curr, wait=True)
-        self.move_group_arm.stop()
-        #TODO
-        pass
+        # Setting the directions.
+        self.s_dir = UDval
+        self.phi_dir = LRval
+
 
     def onTriangleBtnChange(self, val):
         if (val):
@@ -100,11 +80,17 @@ class Robot_Controller:
         # Initialze the robot parameters we want to track.
         self.robot_vel = 0 #[-1,1]
         self.robot_turn = 0 #[-1,1]
-        self.robot_arm_curr_pos = [183.85, 130, 130, math.radians(0), math.radians(90)] # [d, x, y, q1, q2] d, x, y are in mm
+        self.robot_arm_curr_pos = [0, 0, 0] # (s, phi, z). Corresponds to (x, joint1, 1 in lecture calculations).
         self.robot_group_arm_curr = [0, 0, 0, 0] # in radians
-        #TODO functions for parameters below
         self.robot_group_gripper_curr = [0.0, 0.0] # we can open when pressing down on TRIANGLE (?), closes when not
         self.robot_safety = 0 # turns into 1 when SQUARE is pressed once? Back to 0 when pressed again? (With printed message of change
+
+        # Parameters for moving arm.
+        self.s_dir = 0
+        self.phi_dir = 0
+        self.z_dir = 0
+
+
         # initialize robot arm position
         self.move_group_gripper.go(self.robot_group_gripper_curr, wait=True)
         self.move_group_arm.go(self.robot_group_arm_curr, wait=True)
@@ -126,6 +112,47 @@ class Robot_Controller:
          #           print("CAUTION: Obstruction ahead")
 
 
+    def update_arm(self):
+        # The lengths of the arms (maybe)
+        l1 = 0.208
+        l2 = 0.068
+        # Constants to adjust the speed of the robot.
+        ds = 0.05
+        dphi = math.pi/90
+        dz = 0.05
+
+        if (self.s_dir == 0 and self.phi_dir == 0 and self.z_dir == 0):
+            self.move_group_arm.stop()
+            return
+
+        # Update the current positions.
+        self.robot_group_arm_curr = self.move_group_arm.get_current_joint_values()
+        print(self.robot_group_arm_curr)
+
+        # Turning the arm CW/CCW. Phi and joint 1's values are the same.
+        if (self.phi_dir != 0):
+            self.robot_group_arm_curr[0] += dphi * -self.phi_dir
+            if (abs(self.robot_group_arm_curr[0])  > 2.86):
+                self.robot_group_arm_curr[0] = 2.86 * -self.phi_dir
+            self.robot_arm_curr_pos[1] = self.robot_group_arm_curr[0] 
+
+        # TODO: Adjust joints 2 and 3 based on the change in s and z (x and y).
+
+        # TODO: Adjust joint four to always be the sum of joint 2 and 3.
+        # Note that we can also program an offset for this joint to be changed via buttons.
+
+
+
+        # Adjust the motion of the arm.
+        try:
+             self.move_group_arm.go(self.robot_group_arm_curr, wait=False)
+        except(moveit_commander.exception.MoveItCommanderException):
+             print ("Desired motion out of bounds!")
+             self.move_group_arm.stop()
+
+
+
+
     def run(self):
         print ("Controller is initializing...")
         try:
@@ -133,8 +160,8 @@ class Robot_Controller:
                     # Joystick drive
                     leftStickChanged  = self.onLeftStickChange,
                     rightStickChanged = self.onRightStickChange,
-                    rightBtn2Changed = self.onRightBtn2Change,
-                    leftBtn2Changed = self.onLeftBtn2Change,
+                    rightBtn1Changed = self.onRightBtn1Change,
+                    leftBtn1Changed = self.onLeftBtn1Change,
                     hatChanged = self.onHatChange,
                     triangleBtnChanged = self.onTriangleBtnChange,
                     squareBtnChanged = self.onSquareBtnChange)
@@ -152,6 +179,7 @@ class Robot_Controller:
             while keepRunning and not rospy.is_shutdown():
                 # Put stuff here while controller is running.
                 keepRunning = cnt.controllerStatus()
+                self.update_arm()
                 r.sleep()
 
         finally:

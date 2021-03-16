9c5ffcd0d9fc3d97e0980f2e2f12e0d2ca9b2fe7
Kenneth Humphries <humphrik@uchicago.edu>
2021-03-16 15:51:04 -0500

    Added up/down/forward/back motion. Still needs some debugging.

--- scripts/drive_system.py
+++ scripts/drive_system.py
@@ -23,14 +23,14 @@ class Robot_Controller:
     
     def onLeftStickChange(self, LRval, UDval):
         self.robot_vel = -UDval
-        if (abs(self.robot_vel) <= 0.05):
+        if (abs(self.robot_vel) <= 0.06):
             self.robot_vel = 0
         # print ("Left js value changed to ", UDval)
         self.cmd_pub.publish(Vector3(self.robot_vel, 0, 0), Vector3(0, 0, self.robot_turn))
 
     def onRightStickChange(self, LRval, UDval):
         self.robot_turn = -LRval
-        if (abs(self.robot_turn) <= 0.05):
+        if (abs(self.robot_turn) <= 0.06):
             self.robot_turn = 0
         # print ("Right js value changed to ", LRval)
         self.cmd_pub.publish(Vector3(self.robot_vel, 0, 0), Vector3(0, 0, self.robot_turn))
@@ -80,7 +80,12 @@ class Robot_Controller:
         # Initialze the robot parameters we want to track.
         self.robot_vel = 0 #[-1,1]
         self.robot_turn = 0 #[-1,1]
-        self.robot_arm_curr_pos = [0, 0, 0] # (s, phi, z). Corresponds to (x, joint1, 1 in lecture calculations).
+
+        # The arm lengths, maybe.
+        self.l1 = 0.208
+        self.l2 = 0.068
+
+        self.robot_arm_curr_pos = [self.l2, 0, self.l1] # (s, phi, z). Corresponds to (x, joint1, 1 in lecture calculations).
         self.robot_group_arm_curr = [0, 0, 0, 0] # in radians
         self.robot_group_gripper_curr = [0.0, 0.0] # we can open when pressing down on TRIANGLE (?), closes when not
         self.robot_safety = 0 # turns into 1 when SQUARE is pressed once? Back to 0 when pressed again? (With printed message of change
@@ -97,7 +102,13 @@ class Robot_Controller:
         self.move_group_gripper.stop()
         self.move_group_arm.stop()
 
-        rospy.sleep(3)
+        """
+        # Math test.
+        xy = self.get_xy(0, 0)
+        print (xy)
+        qs = self.get_angles(xy[0], xy[1])
+        print (qs)
+        """
 
 
 #    def robot_scan_received(self, data):
@@ -113,13 +124,10 @@ class Robot_Controller:
 
 
     def update_arm(self):
-        # The lengths of the arms (maybe)
-        l1 = 0.208
-        l2 = 0.068
         # Constants to adjust the speed of the robot.
-        ds = 0.05
+        ds = 0.005
         dphi = math.pi/90
-        dz = 0.05
+        dz = 0.005
 
         if (self.s_dir == 0 and self.phi_dir == 0 and self.z_dir == 0):
             self.move_group_arm.stop()
@@ -127,7 +135,7 @@ class Robot_Controller:
 
         # Update the current positions.
         self.robot_group_arm_curr = self.move_group_arm.get_current_joint_values()
-        print(self.robot_group_arm_curr)
+        #print(self.robot_group_arm_curr)
 
         # Turning the arm CW/CCW. Phi and joint 1's values are the same.
         if (self.phi_dir != 0):
@@ -137,9 +145,28 @@ class Robot_Controller:
             self.robot_arm_curr_pos[1] = self.robot_group_arm_curr[0] 
 
         # TODO: Adjust joints 2 and 3 based on the change in s and z (x and y).
+        if (self.s_dir != 0 or self.z_dir != 0):
+            pos = self.get_xy(self.robot_group_arm_curr[1], self.robot_group_arm_curr[2])
+            pos[0] += ds * self.s_dir
+            pos[1] += dz * self.z_dir
+ 
+            qs = self.get_angles(pos[0], pos[1])
+            print ("angles: ", qs)
+            if(not math.isnan(qs[0])):
+                self.robot_group_arm_curr[1] = qs[0]
+                self.robot_group_arm_curr[2] = qs[1]
+            else:
+                print ("Location out of bounds!")
+
+
+
+
 
         # TODO: Adjust joint four to always be the sum of joint 2 and 3.
         # Note that we can also program an offset for this joint to be changed via buttons.
+        #self.robot_group_arm_curr[3] = (math.pi/2 - self.robot_group_arm_curr[1]) - (math.pi/2 + self.robot_group_arm_curr[2])
+
+        self.robot_group_arm_curr[3] = -(self.robot_group_arm_curr[1] + self.robot_group_arm_curr[2])
 
 
 
@@ -151,6 +178,31 @@ class Robot_Controller:
              self.move_group_arm.stop()
 
 
+    def get_angles(self, x, y):
+        l1 = self.l1
+        l2 = self.l2
+        temp2  = ((l1**2) + (l2**2) - (x**2) - (y**2)) / (2*l1*l2)
+        if (abs(temp2) > 1):
+            print ("Point not reachable!")
+            return [float("NaN"), float("NaN")]
+        q2 = -(math.pi -math.acos(temp2))
+        temp1_l = l2 * math.sin(q2)
+        temp1_r = l1 + l2 * math.cos(q2)
+        q1 = math.atan(y/x) - math.atan2(temp1_l, temp1_r)
+
+        # Again, I'm applying the special transormations from problem contexxt to joint angles.
+        return [math.pi/2 - q1, -(math.pi/2 + q2)]
+
+
+
+
+    def get_xy (self, q1, q2):
+        # Note the tranformations on q1 and q2 from the joint angles to the context of this problem.
+        q1prime = math.pi/2 - q1
+        q2prime = -(math.pi/2 + q2)
+        x = self.l1 * math.cos(q1prime) + self.l2 * math.cos(q1prime + q2prime)
+        y = self.l1 * math.sin(q1prime) + self.l2 * math.sin(q1prime + q2prime)
+        return [x, y]
 
 
     def run(self):
